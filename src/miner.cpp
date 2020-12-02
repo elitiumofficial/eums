10b08aa79b2510796e716fc7ac86622b51395549
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2020-12-01 20:30:18 -0500

    Reversal of Amounts
    Reversed nValue from 130,000 to 3,322,000

--- src/miner.cpp
+++ src/miner.cpp
@@ -113,7 +113,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
     txNew.vout[0].scriptPubKey = scriptPubKeyIn;
 
     CBlockIndex* prev = chainActive.Tip();
-    if(prev->nHeight <= 10) txNew.vout[0].nValue = 130000 * COIN;
+    if(prev->nHeight <= 10) txNew.vout[0].nValue = 3322000 * COIN;
     if(prev->nHeight >= 11) txNew.vout[0].nValue = 5 * COIN;
 
 
